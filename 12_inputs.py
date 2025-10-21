a3b2a9647760b5fc6421c25af39721baaf1e9e48
Diego <andre.dev.051223@gmail.com>
2025-10-21 03:51:59 -0500

    clase de inputs

---
+++ 12_inputs.py
@@ -0,0 +1,6 @@
+#Inputs: Hacen referencia a la función input() en Python, que se utiliza para recibir datos del usuario durante la ejecución de un programa. Esta función pausa la ejecución del programa y espera a que el usuario ingrese algún valor, que luego puede ser utilizado dentro del código.
+# Ejemplo de uso de inputs en Python
+
+nombre = input("Por favor, ingresa tu nombre: ")  # Solicita al usuario que ingrese su nombre
+edad = input("Por favor, ingresa tu edad: ")      # Solicita al usuario que ingrese su edad
+print("Hola, " + nombre + "! Tienes " + edad + " años.")  # Muestra un mensaje con el nombre y la edad ingresados
