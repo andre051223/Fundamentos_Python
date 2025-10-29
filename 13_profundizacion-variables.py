3dad07b5263025068da4dcd98916ba3baf21f8e8
Diego <andre.dev.051223@gmail.com>
2025-10-29 06:39:32 -0500

    agregando la clase de profundización de variables

---
+++ 13_profundizacion-variables.py
@@ -0,0 +1,16 @@
+#variables que almacenan las plantillas de dos equipos de fútbol
+plantillaBarcelona = ("Lewandowski", "Lamine Yamal", "Raphinha", "Ferran Torres", "Pedri", "Gavi", "De Jong", "Kounde", "Araujo", "Balde", "Ter Stegen")
+plantillaRealMadrid = ("Benzema", "Vinicius Jr", "Rodrygo", "Modric", "Kroos", "Tchouameni", "Alaba", "Militao", "Carvajal", "Mendy", "Courtois")
+
+print(plantillaBarcelona)
+print(plantillaRealMadrid)
+
+# tuples
+tupla = "Barcelona", "Real Madrid", "Atletico de Madrid", "Sevilla", "Valencia"
+print(tupla)
+
+# conjuntos
+conjunto1 = {"Platzi", "edTeam", "Udemy", "Coursera", "Perfila.ai"}
+conjunto2 = {"Freddy Vega", "Javier Santaolalla", "Gagan Biyani", "Andrew Ng", "Michelle Engelmann"}
+print(conjunto1)
+print(conjunto2)
